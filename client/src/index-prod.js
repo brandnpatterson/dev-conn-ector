84f8abb4566791f9e53c26792799a860b78be3cc
Brandon Patterson <brandnpatterson@gmail.com>
2018-07-05 15:20:16 -0500

    test prod env

---
+++ client/src/index-prod.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { logoutUser, setCurrentUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+
+// Validation
+import jwtDecode from 'jwt-decode';
+import setAuthToken from './validation/setAuthToken';
+
+// Check for jwtToken
+if (localStorage.jwtToken) {
+  // set Auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // Decode token and get User info and exp
+  const decoded = jwtDecode(localStorage.jwtToken);
+
+  // Set User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+
+  if (decoded.exp < currentTime) {
+    // Log out current User
+    store.dispatch(logoutUser());
+
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to Login
+    window.location.href = '/login';
+  }
+}
+
+const renderProd = () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+export default renderProd;
--- client/src/index.js
+++ client/src/index.js
@@ -1,61 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import { AppContainer } from 'react-hot-loader';
+import renderDev from './index-dev';
+import renderProd from './index-prod';
 import registerServiceWorker from './registerServiceWorker';
 
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { logoutUser, setCurrentUser } from './actions/authActions';
-import { clearCurrentProfile } from './actions/profileActions';
+if (process.env.NODE_ENV === 'development') {
+  renderDev();
 
-// Validation
-import jwtDecode from 'jwt-decode';
-import setAuthToken from './validation/setAuthToken';
-
-// Check for jwtToken
-if (localStorage.jwtToken) {
-  // set Auth token header auth
-  setAuthToken(localStorage.jwtToken);
-
-  // Decode token and get User info and exp
-  const decoded = jwtDecode(localStorage.jwtToken);
-
-  // Set User and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-
-  // Check for expired token
-  const currentTime = Date.now() / 1000;
-
-  if (decoded.exp < currentTime) {
-    // Log out current User
-    store.dispatch(logoutUser());
-
-    // Clear current Profile
-    store.dispatch(clearCurrentProfile());
-    // Redirect to Login
-    window.location.href = '/login';
+  if (module.hot) {
+    module.hot.accept('./App', () => renderDev());
   }
+} else if (process.env.NODE_ENV === 'production') {
+  renderProd();
 }
-
-const renderApp = () => {
-  render(
-    <AppContainer>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </AppContainer>,
-    document.getElementById('root')
-  );
-};
 
 registerServiceWorker();
-
-renderApp();
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    renderApp();
-  });
-}
